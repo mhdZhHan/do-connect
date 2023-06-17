6cc1db80fc5d10ceb686fb9226a944dfc1202a80
Mohammed Shajahan <mohammedsh6236@gmail.com>
2023-06-17 13:01:53 +0530

    Start dev Groups

---
+++ client/app/groups/page.js
@@ -0,0 +1,11 @@
+import styles from '@/styles/pages/groups.module.scss'
+
+const Groups = () => {
+    return (
+        <div className="groupsContainer">
+
+        </div>
+    )
+}
+
+export default Groups
