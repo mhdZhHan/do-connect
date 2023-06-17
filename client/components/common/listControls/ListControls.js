e9afc283433292dc1c9c9c1871034c38d66a9627
Mohammed Shajahan <mohammedsh6236@gmail.com>
2023-06-17 10:52:23 +0530

    Update ListControls && global.scss

--- client/components/common/listControls/ListControls.js
+++ client/components/common/listControls/ListControls.js
@@ -1,25 +1,44 @@
-import { BiChevronRight } from 'react-icons/bi'
+"use client"
+import { useState } from 'react'
+import { BiChevronRight, BiChevronDown } from 'react-icons/bi'
 import styles from '@/styles/components/controls.module.scss'
 
 const ListControls = () => {
+    const [isCollapse, setIsCollapse] = useState([true, true])
+
+    const toggleCollapse = (index) => {
+        const updatedCollapseStates = [...isCollapse]
+        updatedCollapseStates[index] = !isCollapse[index]
+        setIsCollapse(updatedCollapseStates)
+    }
+
     return (
         <div className={styles.controlsContainer}>
             <ul className={styles.controlsContainer__options}>
-                <li className={styles.controlsContainer__item}>
+                <li 
+                    className={styles.controlsContainer__item}
+                    onClick={()=>toggleCollapse(0)}
+                >
                     <span className={styles.controlsContainer__label}>all categories</span>
                     <span className={styles.controlsContainer__arrowIcon}>
-                        <BiChevronRight size={20} />
+                        {isCollapse[0] ? <BiChevronRight size={20} /> : <BiChevronDown size={20}/>}
                     </span>
                 </li>
-                <li className={styles.controlsContainer__item}>
+
+                <li 
+                    className={styles.controlsContainer__item}
+                    onClick={()=>toggleCollapse(1)}
+                >
                     <span className={styles.controlsContainer__label}>all tags</span>
                     <span className={styles.controlsContainer__arrowIcon}>
-                        <BiChevronRight size={20} />
+                        {isCollapse[1] ? <BiChevronRight size={20} /> : <BiChevronDown size={20}/>}
                     </span>
                 </li>
+
                 <li className={`${styles.controlsContainer__item} ${styles.hover} ${styles.active}`}>
                     <span className={styles.controlsContainer__label}>Latest</span>
                 </li>
+
                 <li className={`${styles.controlsContainer__item} ${styles.hover}`}>
                     <span className={styles.controlsContainer__label}>Top</span>
                 </li>
